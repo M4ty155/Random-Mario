f22a09dfc1e64256904575c43d9743c771edcb53
M4ty155 <matipollo98@gmail.com>
2024-05-22 11:06:04 -0300

    core: add main menu and wordlist
    Added a main menu where you can choise to play or see a guide about how to play
    
    Added a wordlist where you can delete or edit your wordlist

--- src/main.js
+++ src/main.js
@@ -2,10 +2,34 @@ import { saveInStorage, getWordList, wordlist  } from './storage.js'
 
 let wordArray = []
 let $list = document.querySelector(".show-list")
+let $mainMenu = document.querySelector(".main-menu")
+let $wordList = document.querySelector(".wordlist")
+let $randommize = document.querySelector(".randomize")
+let $buttonWordlist = document.getElementById("goToWordlist")
+
 const $submit = document.getElementById('submit')
 const $word = document.getElementById('word')
+const $playButton = document.getElementById('play')
+
+$wordList.style.display = "none"
+$randommize.style.display = "none"
 
 $submit.addEventListener('click', (e) => onSubmit(e))
+
+$playButton.addEventListener('click', (e) =>{
+    e.preventDefault()
+    $mainMenu.style.display = "none"
+    $wordList.style.display = "block"
+    $randommize.style.display = "block"
+})
+
+$buttonWordlist.addEventListener('click', (e) => {
+    $wordList.style.display = "none"
+    $list.innerHTML = ""
+    for (let i = 0; i < wordArray.length; i++){
+        $list.innerHTML += `<p>${wordArray[i]} <button>Editar</button> <button>Borrar</button></p>`
+    }
+})
 
 function onSubmit(e){
     e.preventDefault()
@@ -18,6 +42,7 @@ function onSubmit(e){
 }
 
 function showList(wordArray){
+    $word.value = ""
     $list.innerHTML = ""
     for (let i = 0; i < wordArray.length; i++){
         $list.innerHTML += `<p>${wordArray[i]}</p>`
